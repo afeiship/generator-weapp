2d7c12d3a95c968f77a2e7ca0f8ac5626c0f46d7
afeiship <1290657123@qq.com>
2018-06-29 15:20:38 +0800

    issue: add componetns

---
+++ generators/component/index.js
@@ -0,0 +1,46 @@
+'use strict';
+const fs = require('fs');
+const rename = require("gulp-rename");
+const path = require('path');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const yoHelper = require('yeoman-generator-helper');
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  prompting(){
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the striking ' + chalk.red('generator-fei-nodejs') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'input',
+      name: 'component_name',
+      message: 'Your component_name?',
+      default: yoHelper.discoverRoot
+    }];
+
+    return this.prompt(prompts).then( (props) => {
+      this.props = props;
+    });
+  }
+
+  writing () {
+    this._writingTplFiles();
+  }
+
+  _writingTplFiles () {
+    yoHelper.rename(this, 'template', 'index');
+    this.fs.copyTpl(
+      this.templatePath(),
+      this.destinationPath(`./components/${this.props.component_name}/`),
+      this.props
+    );
+  }
+
+  install () {
+    console.log('Use `yarn install`');
+    console.log(`Add "components/${this.props.component_name}/index" to your app.json`);
+  }
+};
---
+++ generators/component/templates/index.js
@@ -0,0 +1,16 @@
+Component({
+  properties: {
+    innerText: {
+      type: String,
+      value: 'default value',
+    },
+    img: {
+      type: String,
+      value: 'http://placeholder.qiniudn.com/80x80'
+    },
+    className: {
+      type: String,
+      value: 'cRed'
+    }
+  },
+});
